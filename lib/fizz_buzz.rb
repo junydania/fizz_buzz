b84f9b24bc86b214eca80d2edc5fff287c996d46
junydania <o.dania@aol.com>
2017-03-21 21:01:44 +0100

    Learning Matchers in RSpec

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,6 @@
 
 def fizz_buzz(number)
 
-   
   if zero_remainder?(number,15)
     'Fizz Buzz'
 
@@ -29,7 +28,3 @@ puts usingeach_output.inspect
 usingstep_output = []     #testing step for number iteration
 (0...100).step {|n| usingstep_output << (fizz_buzz(n+1))}
 puts usingstep_output.inspect
-
-usingtimes_output = []     # Testing times for number iteration
-100.times {|n| usingtimes_output << (fizz_buzz(n+1))}
-puts usingtimes_output.inspect
