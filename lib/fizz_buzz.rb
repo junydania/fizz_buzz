2878fe7b85cfbd1b0138049e383af414d6542617
junydania <o.dania@aol.com>
2017-03-21 19:15:44 +0100

    Updated file

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,6 +1,7 @@
 
 def fizz_buzz(number)
 
+   
   if zero_remainder?(number,15)
     'Fizz Buzz'
 
@@ -21,7 +22,14 @@ def zero_remainder?(number, divider)
   number % divider == 0
 end
 
-game_output = []
-(0...100).each {|n| game_output << (fizz_buzz(n+1))}
+usingeach_output = []   #Testing each for number iteration
+(0...100).each {|n| usingeach_output << (fizz_buzz(n+1))}
+puts usingeach_output.inspect
 
-puts game_output.inspect
+usingstep_output = []     #testing step for number iteration
+(0...100).step {|n| usingstep_output << (fizz_buzz(n+1))}
+puts usingstep_output.inspect
+
+usingtimes_output = []     # Testing times for number iteration
+100.times {|n| usingtimes_output << (fizz_buzz(n+1))}
+puts usingtimes_output.inspect
