a12b0435622c0838b7f2291631d9b1da8f7dbb37
junydania <o.dania@aol.com>
2017-03-22 12:05:46 +0100

    Expectation matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,10 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  APP_ROOT = File.expand_path('../..',__FILE__)
+  puts APP_ROOT
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
